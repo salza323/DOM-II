c7896262f41b3cca25b039b403632b0fd9304255
salza323 <sal.zamora480@gmail.com>
2020-09-01 13:13:58 -0700

    mouseover done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Your code goes here
+
+//header
+const header = document.querySelector('.main-navigation')
+console.log(header)
+
+//navigation bar
+const nav = header.querySelector('.nav')
+console.log(nav)
+
+//navigation links
+const links = nav.querySelectorAll('.nav-link')
+console.log(links)
+
+//top container
+const topContainer = document.querySelector('.container.home')
+console.log(topContainer)
+
+//intro
+const intro = topContainer.querySelector('.intro')
+console.log(intro)
+
+//bus picture
+const busPic = intro.querySelector('img')
+console.log(busPic)
+
+//_____________________________________________________________------
+
+//(1)mouseover on nav bar. Change color to orange and revert
+
+nav.addEventListener('mouseover', function (event){
+    event.target.style.color = 'orange'
+    setTimeout(function(){
+        event.target.style.color = ''
+    }, 500)
+}, false)
+
+//(2)
